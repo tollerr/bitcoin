d8dfd1aab221ff6e674273ea89282323dd4abe38
tollerr <rodrigotoller@hotmail.com>
2017-06-17 10:33:21 -0400

    new

--- btc_mbc.py
+++ btc_mbc.py
@@ -15,7 +15,7 @@ coin_url="https://coinbase.com/api/v1/currencies/exchange_rates"
 spot_url="https://coinbase.com/api/v1/prices/spot_rate"
 exch_rate="http://rate-exchange.appspot.com/currency?from=USD&to=BRL" 
 exch_rate="https://www.google.com/search?q=1+usd+in+brl" 
-
+order_book="http://www.mercadobitcoin.com.br/api/orderbook/" 
 #br.set_headers = [(':authority','www.google.com'),(':method','GET'),(':path','/search?q=1+usd+in+brl'),(':scheme','https'),('accept','text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'),('accept-encoding','gzip, deflate, sdch, br'),('accept-language','en-US,en;q=0.8,pt-BR;q=0.6,pt;q=0.4,zh;q=0.2'),('avail-dictionary','ghM1Vvtn'),('cookie','OGPC=765334528-4:278279168-2:560575488-4:976493568-6:946258944-4:908000256-6:864812032-6:391792640-8:193170432-3:699960320-1:448059392-2:1049715712-1:412595200-6:811773952-4:531737600-2:604273664-4:928362496-4:355367936-1:783732736-4:; _ga=GA1.1.1691645100.1493469457; OTZ=3877991_72_76_104100_72_446760; SID=yQSWvHZppeURE-psndA---tKtmp2oy3KwWClkrI36Zf5mv-7XTcgsokxcaNZ9hcNLVF27A.; HSID=AcHvyNI989otpJaUM; SSID=A3rLznVkBAeyv2-pJ; APISID=2-sinx15wzLGBQ5z/Ai5QOIN4CSde2B9JC; SAPISID=s_3XPrvbo_XW4W9g/A7igZuckMp6orZzQu; NID=105=Q7s7Zc3PaRnLLPkyraT2FI5sv3fJDE8Bzs8aSs4t20W10F9seEVwfOvHmZWSCVJghA8i314-TShk9qF3VrikcN9FO67T1tEjUkjw-gViHD_G1HAmsXmtOVhdC0jb-rNtrU2loqpwbzXE4HsQ9TbAoi1SRN4Byu-xAcasX8kH3GAYORsXa4y7uUAyHawkjxBh_NL0pCCqPpbgaHQ4TIeMeAN_kmtq4SX0NdPvblgYbTAHHV6vjv0MJtFjNKWUVRnrZLq0Zj2IbQop8qlexaUs2fPK5Ou4OgCK2CLZTSIFPbRM6JbFWnAMJ5UFqBx8S9mmAjBqDwYzbg0; UULE=a+cm9sZToxIHByb2R1Y2VyOjEyIHByb3ZlbmFuY2U6NiB0aW1lc3RhbXA6MTQ5NzA5Mjc3MjIwMjAwMCBsYXRsbmd7bGF0aXR1ZGVfZTc6NDEwMzU3NjE1IGxvbmdpdHVkZV9lNzotNzM2ODIzNDY4fSByYWRpdXM6NTIwODA=;DV=k2LIO6TzJx9HINXQWGYfNuAqQncbydX9wZ9baRZHcAAAAEBTPR0m8SveeAEAADhOCcNpe3xCXwAAAA'),('upgrade-insecure-requests','1'),('user-agent','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36')]
 
 def fetch_data(url):
@@ -41,49 +41,80 @@ def getCoinbaseData(url):
     for i in idx:
         print "%s - %s"%(i, coin[i])
 
-    return (float (coin['btc_to_usd']), float (coin["btc_to_brl"]), float (coin["usd_to_brl"]) )
+    return (float (coin['btc_to_usd']), float (coin["btc_to_brl"]), float (coin["usd_to_brl"]), float (coin["eth_to_usd"]))
 
 def getMBitcoinData(url):
     data=fetch_data (url)
     return ast.literal_eval(data)["ticker"]["last"] 
 
+
+def applyRates(btc_to_usd, btc_to_brl, usd_to_brl, pos):
+    #btc execution fee = 0.7 forced, 0.3 being executed
+    arbitrage = pos 
+
+    #btc_cash out 2.99
+
+def getRealValue(url,pos,fx):
+
+    data=ast.literal_eval(fetch_data(url))
+    qty=cash=net=total=0
+    mb_ex_fee=.993 #Brazil Execution Fee
+    fx_spread=0.01 #fx spread by the bank
+
+    for ask in data["asks"]:
+        qt=ask[1]
+        pr=ask[0]
+        if qt+qty>pos:
+            qt=pos-qty
+        qty+=qt
+        net=qt*pr*mb_ex_fee
+        total+=net
+        #print "qty:%f, pr:%f, g:%f, net:%f total:(%f,%f)"%(qt,pr,pr*qt,net,qty,total)
+    print "%f\t%f"%(qty,total)
+    to_bank = total- 2.99-(0.0199*total) #mb cash fee R$2.99 + 1.99%
+    transfer_us = to_bank - 115-0.011 * to_bank
+    in_us=transfer_us/fx-(fx*fx_spread)
+    coinbase=in_us-0.0149*in_us
+    print "total:%f, in_itau:%f, transfer:%f, in_us:%f, coin:%f"%(total, to_bank, transfer_us, in_us, coinbase)
+
+    return coinbase
+
+
+
+
+
+
 #Get Coinbase Data
-(btc_to_usd, btc_to_brl, usd_to_brl)=getCoinbaseData(coin_url) 
+(btc_to_usd, btc_to_brl, usd_to_brl, eth_to_usd)=getCoinbaseData(coin_url) 
 
 #get bitcoin from mbc
 mbc_rate=getMBitcoinData(mbc_url)
 
 #get litecoin data
-ltc_rate=getMBitcoinData(ltc_url)
-#print mbc print"btc-mbc- %s"%(mbc"last") print'-' 100
-# print "fetching "+ltc_url
+#ltc_rate=getMBitcoinData(ltc_url)
 
-# br = mechanize.Browser()
-# br.set_handle_robots(False)
-# br.addheaders = [('User-agent', 'Firefox')]
-
-
-# res=br.open(ltc_url)
-# data=res.read()
-# ltc=ast.literal_eval(data)["ticker"]
-# ltc_rate=ltc['last'] 
-# print "ltc-mbc- %s"%(ltc["last"]) 
-
-# print '-' * 100 
-# print "fetching: "+exch_rate
-# #content=br.open(exch_rate).read().decode('UTF-8')
-
-
-#print content
-#soup=BeautifulSoup(content) 
-#d=soup.find('div', attrs= {'class':'vk_ans vk_bk curtgt'}) 
-#data=d.find ('span', attrs={'style':'word-break:break-all'}).string 
-#rate=float(ast.literal_eval(data))
-#print"google rate usd->brl %f"%rate
+pos=1
 rate=usd_to_brl
 arbitrage=mbc_rate-btc_to_brl
 
+real_value=getRealValue(order_book, pos, rate)
+real_arbitrage=real_value-pos*btc_to_usd
+
+print ' --- Bitcoin'
 print'......... arbitrage (br-us):%f BRL, %f USD'%(arbitrage, arbitrage/rate)
 print 'Arbitrage ratio (arb/mbc):%f%%'%(arbitrage/mbc_rate*100)
+print "Opportunity %f btc: %fBRL, %f USD"%(pos, arbitrage*pos, arbitrage/rate*pos)
+print "After fees %f btc: %f USD, %f USD"%(pos, real_value, real_arbitrage)
+print "Arbitrage ration (arb/mbc):%f%%"%(real_arbitrage/btc_to_usd*100)
+#print ' for position:%f , %f BRL, %f USD,  (%f bitcoins)'%(pos, arbitrage*pos, arbitrage/rate*pos, arbitrage/rate*pos/btc_to_usd)
+#bitcoin position
+pos=1.879
+eth_pos=8
+btc_pr=2661
+eth_pr=252.18
 
 #print"%s - %s"%(i, coin(i)) btc. to usd=float(coin ("btc. to usd')
+
+eth_pnl=(eth_pos*eth_to_usd)-(eth_pos*eth_pr)
+btc_pnl=(pos*btc_to_usd)-(pos*btc_pr)
+print 'PNL: %f (btc), %f (eth), Total:%f usd'%(btc_pnl, eth_pnl, btc_pnl+eth_pnl)
